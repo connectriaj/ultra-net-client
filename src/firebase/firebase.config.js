61a501c78129293844267cc89d62cc019449cc91
connectriaj <connect.riaj@hotmail.com>
2022-11-30 02:10:24 +0600

    firebase env adeded

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBwlQ19IhaOjnHxjHAet30Gdf0C66oFVT4",
-  authDomain: "ultra-net-client.firebaseapp.com",
-  projectId: "ultra-net-client",
-  storageBucket: "ultra-net-client.appspot.com",
-  messagingSenderId: "1091597125540",
-  appId: "1:1091597125540:web:859de4aa1073b2894ce331",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
